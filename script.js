2cead1d8d10d7afe0191a69b674af30f8c98e1c7
Jaime Villegas <jaimevillegas296@gmail.com>
2023-02-14 16:42:29 -0500

    Work on refactoring script.js and styling webpage

--- script.js
+++ script.js
@@ -4,57 +4,56 @@ const bookTitle = document.getElementById('book-title');
 const bookAuthor = document.getElementById('book-author');
 const addBook = document.getElementById('add-book');
 
-let books = [];
+class Book {
+  constructor(id, name, author) {
+    this.id = id;
+    this.name = name;
+    this.author = author;
+  }
 
-function createBook(name, author) {
-  const workHTML = document.createElement('section');
-  workHTML.innerHTML = `
-        <p>${name}</p>
-        <p>${author}</p>
-        <button onclick="removeBook('${name}')">Remove</button>
-        <hr>
-    `;
-  SectionBook.appendChild(workHTML);
+  static books = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
+
+  addNewBook() {
+    Book.books.push(this);
+    localStorage.setItem('books', JSON.stringify(Book.books));
+    bookTitle.value = '';
+    bookAuthor.value = '';
+    this.updateList();
+  }
+
+  removeBook(id) {
+    Book.books = Book.books.filter((book) => book.id !== +id);
+    localStorage.setItem('books', JSON.stringify(Book.books));
+    this.updateList();
+  }
+
+  updateList() {
+    SectionBook.innerHTML = '';
+    Book.books.forEach((book) => {
+      SectionBook.insertAdjacentHTML('beforeend', `
+      <article class='book-item'>
+      <p>${book.name}</p>
+      <span> by </span>
+      <p>${book.author}</p>
+      <button class='remove-book btn' data-id='${book.id}'>Remove</button>
+      </article>
+      `);
+    });
+
+    const removeBookBtn = document.querySelectorAll('.remove-book');
+
+    removeBookBtn.forEach((button) => button.addEventListener('click', (e) => {
+      this.removeBook(e.target.dataset.id);
+    }));
+  }
 }
 
-function BookObject(name, author) {
-  this.name = name;
-  this.author = author;
-}
-
-function updateList() {
-  SectionBook.innerHTML = '';
-  books.forEach((book) => {
-    createBook(book.name, book.author);
-  });
-}
-
-function addNewBook(n, a) {
-  const bookObj = new BookObject(n, a);
-  books.push(bookObj);
-  updateList();
-}
-
-function removeBook(title) {
-  books = books.filter((book) => book.name !== title);
-  localStorage.books = JSON.stringify(books);
-  updateList();
-}
+new Book().updateList();
 
 addBook.addEventListener('click', (e) => {
   e.preventDefault();
-  addNewBook(bookTitle.value, bookAuthor.value);
-  localStorage.books = JSON.stringify(books);
-  bookTitle.value = '';
-  bookAuthor.value = '';
+  const newBook = new Book(
+    Math.floor((Math.random() * Date.now())), bookTitle.value, bookAuthor.value,
+  );
+  newBook.addNewBook();
 });
-
-window.addEventListener('load', (() => {
-  SectionBook.innerHTML = '';
-  if (localStorage.books) {
-    books = JSON.parse(localStorage.books);
-  }
-  books.forEach((book) => {
-    createBook(book.name, book.author);
-  });
-}));

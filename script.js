80800450c73d8d346c7ce2fc7b37bdcdad1f23ae
Jaime Villegas <jaimevillegas296@gmail.com>
2023-02-14 08:07:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,86 +1,60 @@
+/* eslint-disable no-unused-vars */
 const SectionBook = document.getElementById('section-book');
-const bookForm = document.getElementById('book-form');
 const bookTitle = document.getElementById('book-title');
 const bookAuthor = document.getElementById('book-author');
 const addBook = document.getElementById('add-book');
 
-const books = [
-    // { name: 'Book title 1',
-    //     author: 'Book author 1',
-    //     id: 0,
-    // },
-    // {
-    //     name: 'Book title 2',
-    //     author: 'Book author 2',
-    //     id: 1,
-    // },
-];
+let books = [];
 
-function createBook(name, author, id) {
-    const workHTML = document.createElement('section');
-    workHTML.innerHTML = `
+function createBook(name, author) {
+  const workHTML = document.createElement('section');
+  workHTML.innerHTML = `
         <p>${name}</p>
         <p>${author}</p>
-        <button id='button-${id}' onclick='getRemoveBook(${id})' class='remove-book'>Remove</button>
+        <button onclick="removeBook('${name}')">Remove</button>
         <hr>
     `;
-    SectionBook.appendChild(workHTML);
+  SectionBook.appendChild(workHTML);
 }
 
-function bookObject(name, author, id) {
-    this.name = name;
-    this. author = author;
-    this.id = id;
+function BookObject(name, author) {
+  this.name = name;
+  this.author = author;
 }
 
-function addNewBook(n, a, i) {
-    var bookObj = new bookObject(n, a, i);
-    books.push(bookObj);
-    SectionBook.innerHTML = '';
-    books.forEach((book) => {
-        createBook(book.name, book.author, book.id);
-    });
+function updateList() {
+  SectionBook.innerHTML = '';
+  books.forEach((book) => {
+    createBook(book.name, book.author);
+  });
+}
+
+function addNewBook(n, a) {
+  const bookObj = new BookObject(n, a);
+  books.push(bookObj);
+  updateList();
+}
+
+function removeBook(title) {
+  books = books.filter((book) => book.name !== title);
+  localStorage.books = JSON.stringify(books);
+  updateList();
 }
 
 addBook.addEventListener('click', (e) => {
-    e.preventDefault();
-    var idNumber = books.length + 1;
-    addNewBook(bookTitle.value, bookAuthor.value, idNumber)
-})
+  e.preventDefault();
+  addNewBook(bookTitle.value, bookAuthor.value);
+  localStorage.books = JSON.stringify(books);
+  bookTitle.value = '';
+  bookAuthor.value = '';
+});
 
 window.addEventListener('load', (() => {
-    SectionBook.innerHTML = '';
-    books.forEach((book) => {
-        createBook(book.name, book.author, book.id);
-    });
-}));
-
-function getRemoveBook(id) {
-  let newBooks = books.forEach((book) => {
-    if (book.id === id) {
-      console.log(book.id);
-      delete books[id - 1];
-      console.log(books);
-      SectionBook.innerHTML = '';
-      books.forEach((book) => {
-          createBook(book.name, book.author, book.id);
-      });
-    }
-    // console.log(book.id);
-    // return book.id == id;
+  SectionBook.innerHTML = '';
+  if (localStorage.books) {
+    books = JSON.parse(localStorage.books);
+  }
+  books.forEach((book) => {
+    createBook(book.name, book.author);
   });
-  // console.log('Hello from getRemoveBooks');
-  // console.log(id);
-  // console.log(newBooks);
-};
-
-// const removedBook = document.getElementById(`book-${id}`);
-// removedBook.addEventListener('click', getRemoveBook);
-console.log("hello");
-
-// const removeBook = document.getElementsByClassName('remove-book');
-
-// removeBook.addEventListener('click', (e) => {
-//   e.preventDefault();
-// })
-
+}));
